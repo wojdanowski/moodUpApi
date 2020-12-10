93308b0c4c12d72ceb694e482106944b461c7b39
wojdanowski <s.wojdanowski@gmail.com>
2020-12-10 20:01:15 +0100

    get all recipes by user id

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -12,18 +12,18 @@ exports.getOne = (Model) =>
 		});
 	});
 
-exports.getAll = (Model) =>
-	catchAsync(async (req, res, next) => {
-		const doc = await Model.find();
+// exports.getAll = (Model) =>
+// 	catchAsync(async (req, res, next) => {
+// 		const doc = await Model.find();
 
-		res.status(200).json({
-			status: 'success',
-			results: doc.length,
-			data: {
-				data: doc,
-			},
-		});
-	});
+// 		res.status(200).json({
+// 			status: 'success',
+// 			results: doc.length,
+// 			data: {
+// 				data: doc,
+// 			},
+// 		});
+// 	});
 
 exports.createOne = (Model) =>
 	catchAsync(async (req, res, next) => {
--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,6 +1,19 @@
 const Recipe = require('./../models/recipeModel');
 const factory = require('./../controllers/handlerFactory');
+const catchAsync = require('./../utils/catchAsync');
 
 exports.getRecipe = factory.getOne(Recipe);
-exports.getAllRecipes = factory.getAll(Recipe);
+// exports.getAllRecipes = factory.getAll(Recipe);
 exports.createRecipe = factory.createOne(Recipe);
+
+exports.getAllRecipes = catchAsync(async (req, res, next) => {
+	const doc = await Recipe.find({ author: '5fd2689c081e33f052b9fdc7' });
+
+	res.status(200).json({
+		status: 'success',
+		results: doc.length,
+		data: {
+			data: doc,
+		},
+	});
+});
