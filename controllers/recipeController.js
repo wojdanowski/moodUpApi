2185ee5066e0729aef3e46f8080dcda43a18f152
wojdanowski <s.wojdanowski@gmail.com>
2020-12-10 17:42:38 +0100

    basic recipe Model

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,10 +1,12 @@
 const catchAsync = require('./../utils/catchAsync');
+const Recipe = require('./../models/recipeModel');
 
 exports.getRecipe = catchAsync(async (req, res, next) => {
 	console.log(`[recipeController] get recipe`);
-	res.send('Get Recipe');
-	// res.status(204).json({
-	// 	status: 'success',
-	// 	data: { recipes: 'all recipes' },
-	// });
+	console.log(req.params);
+	const document = await Recipe.find();
+	res.status(200).json({
+		status: 'success',
+		data: document,
+	});
 });
---
+++ models/recipeModel.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, 'A recipe must have a name'],
+			unique: true,
+			trim: true,
+			maxlength: [
+				40,
+				'A recipe name must have less or equal 40 characters',
+			],
+			minlength: [
+				3,
+				'A recipe name must have more or equal 3 characters',
+			],
+		},
+		shortDescription: {
+			type: String,
+			required: [true, 'A recipe must have a short description'],
+			unique: false,
+			trim: true,
+			maxlength: [
+				10,
+				'A recipe short description must have less or equal 40 characters',
+			],
+			minlength: [
+				1000,
+				'A recipe short description must have more or equal 1000 characters',
+			],
+		},
+		prepTime: {
+			type: Number,
+			required: [true, 'A recipe must have a preparation time'],
+			unique: false,
+			trim: true,
+			maxlength: [
+				10,
+				'A recipe short description must have less or equal 40 characters',
+			],
+			minlength: [
+				1000,
+				'A recipe short description must have more or equal 1000 characters',
+			],
+		},
+		prepSteps: [
+			{
+				type: String,
+				required: [true, 'A recipe must have preparation steps'],
+			},
+		],
+		ingredients: [
+			{
+				name: String,
+				quantity: String,
+			},
+		],
+		author: {
+			type: mongoose.Schema.ObjectId,
+			ref: 'User',
+			required: [true, 'A recipe must have an author'],
+		},
+	},
+	{
+		toJSON: { virtuals: true },
+		toObject: { virtuals: true },
+	}
+);
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+module.exports = Recipe;
