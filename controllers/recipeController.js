4efa577077b01b5283e6c3d11c860e0958257fc4
wojdanowski <s.wojdanowski@gmail.com>
2020-12-10 20:27:14 +0100

    errar handlin basic bugfix and imports

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,6 +1,7 @@
 const Recipe = require('./../models/recipeModel');
 const factory = require('./../controllers/handlerFactory');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.getRecipe = factory.getOne(Recipe);
 exports.createRecipe = factory.createOne(Recipe);
@@ -12,6 +13,10 @@ exports.getAllRecipes = catchAsync(async (req, res, next) => {
 		},
 		'name prepTime'
 	);
+
+	if (!doc) {
+		return next(new AppError('No document found with that ID', 404));
+	}
 
 	res.status(200).json({
 		status: 'success',
