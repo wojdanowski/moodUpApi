25d817a833c411f2259f5b6be9cf87c170d3d35e
wojdanowski <s.wojdanowski@gmail.com>
2020-12-11 10:21:45 +0100

    protect midlleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const catchAsync = require('./../utils/catchAsync');
+const { promisify } = require('util');
 const User = require('./../models/userModel');
 const AppError = require('./../utils/appError');
 
@@ -61,4 +62,40 @@ exports.login = catchAsync(async (req, res, next) => {
 
 	//  3) If everything is OK, send token to client
 	createSendToken(user, 200, res);
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+	// 1) Getting token and check if it's there
+	let token;
+	if (
+		req.headers.authorization &&
+		req.headers.authorization.startsWith('Bearer')
+	) {
+		token = req.headers.authorization.split(' ')[1];
+	} else if (req.cookies.jwt) {
+		token = req.cookies.jwt;
+	}
+
+	if (!token) {
+		return next(
+			new AppError(
+				'You are not logged in! Please log in to get access.',
+				401
+			)
+		);
+	}
+
+	// 2) Verification of token
+	const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+	console.log(decoded);
+
+	// 3) Check if user still exists
+	const currentUser = await User.findById(decoded.id);
+	if (!currentUser) {
+		return next(new AppError('The user no longer exists.', 401));
+	}
+
+	// GRANT ACCESS TO PROTECTED ROUTE
+	req.user = currentUser;
+	next();
 });
--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const recipeController = require('./../controllers/recipeController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
+
+router.use(authController.protect);
 
 router
 	.route('/')
