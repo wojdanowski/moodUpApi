232dc7fbcb4d7656e8937a767d10e407138a9c4b
Sebastian <sebastian@Sebastians-MacBook-Air.local>
2021-01-20 08:36:37 +0100

    auth mockup in appTest.spec.js finally works

--- tests/appTest.spec.js
+++ tests/appTest.spec.js
@@ -1,9 +1,7 @@
-// Import the dependencies for testing
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const sinon = require('sinon');
 const { StatusCodes } = require('http-status-codes');
-const authController = require('./../controllers/authController');
 const decache = require('decache');
 
 // Configure chai
@@ -11,35 +9,53 @@ chai.use(chaiHttp);
 chai.should();
 
 describe('Recipes', () => {
+	let app;
+	let authController;
+	beforeEach(function () {
+		authController = require('./../controllers/authController');
+		sinon.stub(authController, 'protect').callsFake((req, res, next) => {
+			req.user = {
+				id: '5fd346ddb5a83f42fc226565',
+				role: 'admin',
+			};
+			return next();
+		});
+		app = require('./../server').app;
+	});
+
+	afterEach(function () {
+		authController.protect.restore();
+	});
+
 	describe('GET /', () => {
-		let app;
-
-		beforeEach(function () {
-			sinon
-				.stub(authController, 'protect')
-				.callsFake((req, res, next) => {
-					req.user = {
-						id: '5fd346ddb5a83f42fc226565',
-						role: 'admin',
-					};
-					next();
-				});
-		});
-
-		beforeEach(function () {
-			decache('./../server');
-			console.log(`decache!!!!!`);
-			app = require('./../server').app;
-		});
-
-		afterEach(function () {
-			authController.protect.restore();
-		});
-
-		it('should get all recipes', async function () {
+		it('should get all recipes and return status code 200', async function () {
 			const response = await chai.request(app).get('/api/v1/recipes/');
 
 			response.should.have.status(StatusCodes.OK);
+			response.body.should.be.a('object');
+		});
+
+		it('should get one recipe and return status code 200  ', async function () {
+			const response = await chai
+				.request(app)
+				.get('/api/v1/recipes/5fd790724a7ab216c8920314');
+			response.should.have.status(StatusCodes.OK);
+			response.body.should.be.a('object');
+		});
+
+		it('should return status code 404', async function () {
+			const response = await chai
+				.request(app)
+				.get('/api/v1/recipes/5fd790724a7ab216c8920315');
+			response.should.have.status(StatusCodes.NOT_FOUND);
+			response.body.should.be.a('object');
+		});
+
+		it('should return status code 400', async function () {
+			const response = await chai
+				.request(app)
+				.get('/api/v1/recipes/5fd790724a7ab216!c8920315');
+			response.should.have.status(StatusCodes.NOT_FOUND);
 			response.body.should.be.a('object');
 		});
 	});
