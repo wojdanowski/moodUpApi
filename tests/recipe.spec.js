4c416c70dadca1fd6c39d76e49f168f0b58a849f
Sebastian <sebastian@Sebastians-MacBook-Air.local>
2021-01-20 10:29:25 +0100

    changes after code review #19

--- tests/recipe.spec.js
+++ tests/recipe.spec.js
@@ -1,24 +1,18 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const sinon = require('sinon');
+const { chai } = require('./test_config');
 const { StatusCodes } = require('http-status-codes');
+const { authMockup } = require('./authMock');
 
-// Configure chai
-chai.use(chaiHttp);
-chai.should();
+const BASE_URL = '/api/v1/recipes';
+const RECIPE_ID_CORRECT = '5fd790724a7ab216c8920314';
+const RECIPE_ID_NOT_EXISTENT = '5fd790724a7ab216c8920315';
+const RECIPE_ID_WRONG = '5fd790724a7ab216!c8920315';
 
 describe('Recipes', () => {
 	let app;
 	let authController;
 	beforeEach(function () {
 		authController = require('./../controllers/authController');
-		sinon.stub(authController, 'protect').callsFake((req, res, next) => {
-			req.user = {
-				id: '5fd346ddb5a83f42fc226565',
-				role: 'admin',
-			};
-			return next();
-		});
+		authMockup(authController);
 		app = require('./../server').app;
 	});
 
@@ -28,7 +22,7 @@ describe('Recipes', () => {
 
 	describe('GET /', () => {
 		it('should get all recipes and return status code 200', async function () {
-			const response = await chai.request(app).get('/api/v1/recipes/');
+			const response = await chai.request(app).get(BASE_URL);
 
 			response.should.have.status(StatusCodes.OK);
 			response.body.should.be.a('object');
@@ -37,7 +31,7 @@ describe('Recipes', () => {
 		it('should get one recipe and return status code 200  ', async function () {
 			const response = await chai
 				.request(app)
-				.get('/api/v1/recipes/5fd790724a7ab216c8920314');
+				.get(`${BASE_URL}/${RECIPE_ID_CORRECT}`);
 			response.should.have.status(StatusCodes.OK);
 			response.body.should.be.a('object');
 		});
@@ -45,7 +39,7 @@ describe('Recipes', () => {
 		it('should return status code 404', async function () {
 			const response = await chai
 				.request(app)
-				.get('/api/v1/recipes/5fd790724a7ab216c8920315');
+				.get(`${BASE_URL}/${RECIPE_ID_NOT_EXISTENT}`);
 			response.should.have.status(StatusCodes.NOT_FOUND);
 			response.body.should.be.a('object');
 		});
@@ -53,7 +47,7 @@ describe('Recipes', () => {
 		it('should return status code 400', async function () {
 			const response = await chai
 				.request(app)
-				.get('/api/v1/recipes/5fd790724a7ab216!c8920315');
+				.get(`${BASE_URL}/${RECIPE_ID_WRONG}`);
 			response.should.have.status(StatusCodes.BAD_REQUEST);
 			response.body.should.be.a('object');
 		});
