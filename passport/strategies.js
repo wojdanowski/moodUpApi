6572280fb68290d0c0c452b7b216b5c08d6905b5
Sebastian Wojdanowski <s.wojdanowski@gmail.com>
2021-01-26 14:44:23 +0100

    pRefactor redis calls in strategies.js to use Promises, Added expiration to records

--- passport/strategies.js
+++ passport/strategies.js
@@ -2,46 +2,45 @@ const BearerStrategy = require('passport-http-bearer').Strategy;
 const jwt = require('jsonwebtoken');
 const { promisify } = require('util');
 const User = require('./../models/userModel');
-const { redisClient } = require('./../redis');
+const { getAsync, setAsync } = require('./../redis');
 
 const authorizeToken = async (req, accessToken, callback) => {
 	try {
 		// Verify if token is in cache
-		redisClient.get(accessToken, async (err, reply) => {
-			let currentUser;
-			if (reply) {
-				currentUser = JSON.parse(reply);
-				console.log(`Sending user from redis`);
-				return callback(null, currentUser, {
-					scope: '*',
-				});
-			} else {
-				// Verification of token
-				const decoded = await promisify(jwt.verify)(
-					accessToken,
-					process.env.JWT_SECRET
-				);
-				// Check if user still exists
-				currentUser = await User.findById(decoded.id);
-				if (!currentUser) {
-					return next(
-						new AppError(
-							'The user no longer exists.',
-							StatusCodes.UNAUTHORIZED
-						)
-					);
-				}
-				redisClient.set(
-					accessToken,
-					JSON.stringify(currentUser),
-					(err, reply) => {
-						console.log(`setting new token to redis`);
-						return callback(null, currentUser, {
-							scope: '*',
-						});
-					}
-				);
-			}
+		let currentUser;
+		const cachedUser = await getAsync(accessToken);
+		if (cachedUser) {
+			return callback(null, JSON.parse(cachedUser), {
+				scope: '*',
+			});
+		}
+
+		// Verification of token
+		const decoded = await promisify(jwt.verify)(
+			accessToken,
+			process.env.JWT_SECRET
+		);
+
+		// Check if user still exists
+		currentUser = await User.findById(decoded.id);
+		if (!currentUser) {
+			return next(
+				new AppError(
+					'The user no longer exists.',
+					StatusCodes.UNAUTHORIZED
+				)
+			);
+		}
+
+		// Cache user in redis and send response to client
+		await setAsync(
+			accessToken,
+			JSON.stringify(currentUser),
+			'EX',
+			process.env.REDIS_EXPIRES_IN
+		);
+		return callback(null, currentUser, {
+			scope: '*',
 		});
 	} catch (e) {
 		return callback(null, false);
