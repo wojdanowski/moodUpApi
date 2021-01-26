147c81bb02454e31619340c86d84b5330c538f2a
Sebastian Wojdanowski <s.wojdanowski@gmail.com>
2021-01-26 11:29:32 +0100

    Added basic redis setup

--- passport/strategies.js
+++ passport/strategies.js
@@ -2,16 +2,22 @@ const BearerStrategy = require('passport-http-bearer').Strategy;
 const jwt = require('jsonwebtoken');
 const { promisify } = require('util');
 const User = require('./../models/userModel');
+const redisUsers = require('./../server');
 
 const authorizeToken = async (req, accessToken, callback) => {
 	try {
-		// 2) Verification of token
+		// Verify if token is in cache
+		await redis.set('foo', 'bar');
+		const redisRes = await redis.get('foo');
+		console.log(redisRes);
+
+		// Verification of token
 		const decoded = await promisify(jwt.verify)(
 			accessToken,
 			process.env.JWT_SECRET
 		);
 
-		// // 3) Check if user still exists
+		// Check if user still exists
 		const currentUser = await User.findById(decoded.id);
 		if (!currentUser) {
 			return next(
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const terminate = require('./utils/terminate');
+const Redis = require('ioredis');
 
 process.on('uncaughtException', (err) => {
 	console.log(`Unhandled exception! Shutting down...`);
@@ -24,6 +25,14 @@ mongoose
 	})
 	.then(() => console.log(`DB connection successful`));
 
+let redis_url = process.env.REDIS_URL;
+let redis_port = process.env.REDIS_PORT;
+if (process.env.NODE_ENV === 'development') {
+	redis_url = 'redis://127.0.0.1';
+	redis_port = 6379;
+}
+const redisUsers = new Redis();
+
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
 	console.log(`App running on port: ${port}...`);
@@ -39,4 +48,7 @@ process.on('unhandledRejection', exitHandler(1, 'Unhandled Promise'));
 process.on('SIGTERM', exitHandler(0, 'SIGTERM'));
 process.on('SIGINT', exitHandler(0, 'SIGINT'));
 
-module.exports.app = app;
+module.exports.app = {
+	app,
+	redisUsers,
+};
