59e97febcf87ee99a692885bcc6c2a23b524b65d
Sebastian Wojdanowski <s.wojdanowski@gmail.com>
2021-01-28 10:14:58 +0100

    Refactor handlerFactory.js to typescript

--- src/controllers/handlerFactory.ts
+++ src/controllers/handlerFactory.ts
@@ -1,9 +1,9 @@
 const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
-const { StatusCodes } = require('http-status-codes');
+import AppError from './../utils/appError';
+import { StatusCodes } from 'http-status-codes';
 
-exports.deleteOne = (Model) =>
-	catchAsync(async (req, res, next) => {
+const deleteOne = (Model: any) =>
+	catchAsync(async (req: any, res: any, next: any) => {
 		const doc = await Model.findByIdAndDelete(req.params.id);
 
 		if (!doc) {
@@ -20,3 +20,5 @@ exports.deleteOne = (Model) =>
 			data: null,
 		});
 	});
+
+export { deleteOne };
