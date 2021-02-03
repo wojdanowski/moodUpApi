a895fc91ea64efcffb3ba36edd79507c3eb6727f
Sebastian Wojdanowski <s.wojdanowski@gmail.com>
2021-02-03 14:41:25 +0100

    Added types to apiFeatures

--- src/utils/apiFeatures.ts
+++ src/utils/apiFeatures.ts
@@ -7,28 +7,28 @@ interface IQueryOptions {
 }
 
 export default class APIFeatures {
-	query: DocumentQuery<Document[], Document>;
-	queryString: IQueryOptions;
+	public query: DocumentQuery<Document[], Document>;
+	private options: IQueryOptions;
 
-	constructor(
+	public constructor(
 		query: DocumentQuery<Document[], Document>,
-		queryString: IQueryOptions
+		options: IQueryOptions
 	) {
 		this.query = query;
-		this.queryString = queryString;
+		this.options = options;
 	}
 
-	paginate() {
-		const page = this.queryString.page ? this.queryString.page * 1 : 1;
-		const limit = this.queryString.limit ? this.queryString.limit * 1 : 100;
+	public paginate(): this {
+		const page = this.options.page ? this.options.page * 1 : 1;
+		const limit = this.options.limit ? this.options.limit * 1 : 100;
 		const skip = (page - 1) * limit;
 		this.query = this.query.skip(skip).limit(limit);
 		return this;
 	}
 
-	search() {
-		if (this.queryString.search) {
-			const phrase = this.queryString.search;
+	public search(): this {
+		if (this.options.search) {
+			const phrase = this.options.search;
 			this.query = this.query.find({
 				name: new RegExp(phrase, 'i'),
 			});
