7cdbeabbb9e9a65476067f554d3269feef15d611
Sebastian Wojdanowski <s.wojdanowski@gmail.com>
2021-02-03 10:04:29 +0100

    Added types to catchAsync.ts

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,5 +1,15 @@
-export default (fn: any) => {
-	return (req: any, res: any, next: any) => {
+import { NextFunction, Request, Response } from 'express';
+
+interface ReturnedFunc {
+	(req: Request, res: Response, next: NextFunction): void;
+}
+
+interface InputFunc {
+	(...[req, res, next]: Parameters<ReturnedFunc>): Promise<void>;
+}
+
+export default (fn: InputFunc): ReturnedFunc => {
+	return (req: Request, res: Response, next: NextFunction): void => {
 		fn(req, res, next).catch(next);
 	};
 };
