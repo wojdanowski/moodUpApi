4164e205fdd99ecd7a819c02fd0685baa7d91694
Sebastian Wojdanowski <s.wojdanowski@gmail.com>
2021-02-09 13:10:04 +0100

    Reformat recipe.spec.ts eslint prettier

--- src/tests/recipe.spec.ts
+++ src/tests/recipe.spec.ts
@@ -3,75 +3,59 @@ import { StatusCodes } from 'http-status-codes';
 import { getUserData, getDummyRecipe, TestUser } from './mockDb';
 import { IRecipeTemplate } from '../models/recipeModel';
 import { Request, Response, NextFunction } from 'express';
-import { Response as SuperResponse } from 'superagent';
 
 const BASE_URL: string = '/api/v1/recipes';
 const RECIPE_ID_NOT_EXISTENT: string = '5fd790724a7ab216c8920315';
 const RECIPE_ID_WRONG: string = 'testId';
 
 describe('Recipes', (): void => {
-	let dummyUser: TestUser;
-	let dummyRecipe: IRecipeTemplate;
-	const EXPECTED_RECIPE_COUNT: number = 2;
+  let dummyUser: TestUser;
+  let dummyRecipe: IRecipeTemplate;
+  const EXPECTED_RECIPE_COUNT: number = 2;
 
-	before(async function (): Promise<void> {
-		dummyUser = await getUserData();
-		dummyRecipe = await getDummyRecipe();
-	});
+  before(async function (): Promise<void> {
+    dummyUser = await getUserData();
+    dummyRecipe = await getDummyRecipe();
+  });
 
-	describe('GET /', (): void => {
-		beforeEach(function (): void {
-			stubbedIsAuthenticatedApi.callsFake(
-				(req: Request, res: Response, next: NextFunction): void => {
-					req.user = {
-						...dummyUser,
-					};
-					return next();
-				}
-			);
-		});
+  describe('GET /', (): void => {
+    beforeEach(function (): void {
+      stubbedIsAuthenticatedApi.callsFake((req: Request, res: Response, next: NextFunction): void => {
+        req.user = {
+          ...dummyUser,
+        };
+        return next();
+      });
+    });
 
-		it('should return status code 200', async function (): Promise<void> {
-			const response: SuperResponse = await chai
-				.request(app)
-				.get(BASE_URL);
+    it('should return status code 200', async function (): Promise<void> {
+      const response = await chai.request(app).get(BASE_URL);
 
-			response.should.have.status(StatusCodes.OK);
-		});
+      response.should.have.status(StatusCodes.OK);
+    });
 
-		it('should return all (two) recipes from database', async function (): Promise<void> {
-			const response: SuperResponse = await chai
-				.request(app)
-				.get(BASE_URL);
+    it('should return all (two) recipes from database', async function (): Promise<void> {
+      const response = await chai.request(app).get(BASE_URL);
 
-			chai.assert(
-				response.body.results === EXPECTED_RECIPE_COUNT,
-				'Returned invalid number of recipes from database'
-			);
-		});
+      chai.assert(response.body.results === EXPECTED_RECIPE_COUNT, 'Returned invalid number of recipes from database');
+    });
 
-		it('should get recipe with provided ID and return status code 200  ', async function (): Promise<void> {
-			const response: SuperResponse = await chai
-				.request(app)
-				.get(`${BASE_URL}/${dummyRecipe._id}`);
+    it('should get recipe with provided ID and return status code 200  ', async function (): Promise<void> {
+      const response = await chai.request(app).get(`${BASE_URL}/${dummyRecipe._id}`);
 
-			response.should.have.status(StatusCodes.OK);
-		});
+      response.should.have.status(StatusCodes.OK);
+    });
 
-		it('should return status code 404 when there is no recipe with provided ID', async function (): Promise<void> {
-			const response: SuperResponse = await chai
-				.request(app)
-				.get(`${BASE_URL}/${RECIPE_ID_NOT_EXISTENT}`);
+    it('should return status code 404 when there is no recipe with provided ID', async function (): Promise<void> {
+      const response = await chai.request(app).get(`${BASE_URL}/${RECIPE_ID_NOT_EXISTENT}`);
 
-			response.should.have.status(StatusCodes.NOT_FOUND);
-		});
+      response.should.have.status(StatusCodes.NOT_FOUND);
+    });
 
-		it('should return status code 400 when provided with invalid/not-MongoDb ID', async function (): Promise<void> {
-			const response: SuperResponse = await chai
-				.request(app)
-				.get(`${BASE_URL}/${RECIPE_ID_WRONG}`);
+    it('should return status code 400 when provided with invalid/not-MongoDb ID', async function (): Promise<void> {
+      const response = await chai.request(app).get(`${BASE_URL}/${RECIPE_ID_WRONG}`);
 
-			response.should.have.status(StatusCodes.BAD_REQUEST);
-		});
-	});
+      response.should.have.status(StatusCodes.BAD_REQUEST);
+    });
+  });
 });
