76243b9c5800a4bb2bc5c66086fc3fb47859871f
Sebastian Wojdanowski <s.wojdanowski@gmail.com>
2021-02-09 13:12:48 +0100

    Reformat docsController eslint prettier

--- src/controllers/docsController.ts
+++ src/controllers/docsController.ts
@@ -3,16 +3,16 @@ import { catchAsync } from '../utils/catchAsync';
 import path from 'path';
 
 const getApiDocumentation = catchAsync(
-	async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-		const options = {
-			headers: {
-				'Content-Security-Policy':
-					"default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'",
-			},
-		};
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const options = {
+      headers: {
+        'Content-Security-Policy':
+          "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'",
+      },
+    };
 
-		res.sendFile(path.join(__dirname, '../public/index.html'), options);
-	}
+    res.sendFile(path.join(__dirname, '../public/index.html'), options);
+  },
 );
 
 export { getApiDocumentation };
