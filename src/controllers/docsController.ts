bb97f3a94017fa1a4bd7ec519e078468c377ee7a
Sebastian Wojdanowski <s.wojdanowski@gmail.com>
2021-02-12 13:48:05 +0100

    Refactor docsController

--- src/controllers/docsController.ts
+++ src/controllers/docsController.ts
@@ -1,17 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import { catchAsync } from '../utils/catchAsync';
 import path from 'path';
+import { CONTENT_SEC_POLICY } from './../utils/headers';
 
 const getApiDocumentation = catchAsync(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    const options = {
-      headers: {
-        'Content-Security-Policy':
-          "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'",
-      },
-    };
-
-    res.sendFile(path.join(__dirname, '../public/index.html'), options);
+    res.sendFile(path.join(__dirname, '../public/index.html'), { headers: CONTENT_SEC_POLICY });
   },
 );
 
