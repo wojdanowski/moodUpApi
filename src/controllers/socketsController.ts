42487868e7ccb0a0ff869e233faaf7745d1275c1
Sebastian Wojdanowski <s.wojdanowski@gmail.com>
2021-02-17 13:22:05 +0100

    Renamed imports in socketController.ts

--- src/controllers/socketsController.ts
+++ src/controllers/socketsController.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import { catchAsync } from '../utils/catchAsync';
 import path from 'path';
-import { CONTENT_SEC_POLICY } from './../utils/headers';
+import { CONTENT_SECURITY_POLICY } from './../utils/headers';
 
 const getSocketTestPage = catchAsync(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    res.sendFile(path.join(__dirname, '../public/sockets_test_html.html'), { headers: CONTENT_SEC_POLICY });
+    res.sendFile(path.join(__dirname, '../public/sockets_test_html.html'), { headers: CONTENT_SECURITY_POLICY });
   },
 );
 
