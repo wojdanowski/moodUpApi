ffb00cb6839636bade3199961f448899bf5d76dd
Sebastian Wojdanowski <s.wojdanowski@gmail.com>
2021-02-17 13:16:44 +0100

    Renamed import in docsController

--- src/controllers/docsController.ts
+++ src/controllers/docsController.ts
@@ -1,11 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import { catchAsync } from '../utils/catchAsync';
 import path from 'path';
-import { CONTENT_SEC_POLICY } from './../utils/headers';
+import { CONTENT_SECURITY_POLICY } from './../utils/headers';
 
 const getApiDocumentation = catchAsync(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    res.sendFile(path.join(__dirname, '../public/index.html'), { headers: CONTENT_SEC_POLICY });
+    res.sendFile(path.join(__dirname, '../public/index.html'), { headers: CONTENT_SECURITY_POLICY });
   },
 );
 
